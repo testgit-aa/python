949bbd62a6923539f1fa9fa3ad1ac492094c14e8
Axel <axelalmquist@gmail.com>
2020-11-25 13:19:42 +0100

    import task fix

--- tasks/import1.py
+++ tasks/import1.py
@@ -1,7 +1,7 @@
 
 
 """
-Add depth to the current package stucture (src) so that the import statement bellow words.
+Add depth to the current package stucture (tasks) so that the import statement bellow words.
 
 restrictions:
 1) shallow, deeper, deepest, too_deep should all be folders
@@ -13,7 +13,7 @@ restrictions:
 3) deepseafish() should be located in a file called "something_in_the_deep.py"
 """
 
-from src.shallow.deeper.deepest.too_deep import deepseafish
+from tasks.shallow.deeper.deepest.too_deep import deepseafish
 
 def whats_at_the_bottom():
     return deepseafish()
